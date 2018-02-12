e69e4cc05e215d6137f39cdecdeeab55564ae4d7
Trina Haque <trina_haque28@outlook.com>
2018-02-12 11:39:52 -0800

    script for curling csv file

--- script.py
+++ script.py
@@ -6,15 +6,28 @@
 # Write the output in a seperate text file
 
 
-import csv
+import csv, requests, os
+
+def get(url):
+    cmd = "curl -s -m 10 -o /dev/null -w '%%{http_code}' %s" % url
+    print(cmd)
+    # look up what os.popen do
+    return os.popen("curl -s -o /dev/null -w '%%{http_code}' %s" % url).read()
+     
 
 with open('sub-domains.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
+    # next escapes the header
     next(csv_reader)
     
-    with open('curl.csv', 'w') as new_file:
+    with open('curl.csv', 'w', newline='') as new_file:
         csv_writer = csv.writer(new_file)
         
         for line in csv_reader:
-            csv_writer.writerow([line[0]])
+            url = "https://"+line[0]
+            resp = get(url)
+            csv_writer.writerow([url, resp])
+            url = "http://"+line[0]
+            resp = get(url)
+            csv_writer.writerow([url, resp])
